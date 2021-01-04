5c4e26a466753aa7dfd376446d7e85e27e253f84
Joshua Sinyor <josh.sinyor@gmail.com>
2021-01-04 18:31:09 +0000

    Updated fizzbuzz.rb and fizzbuzz_spec.rb. Includes more tests.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,6 +2,24 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(3)).to eq 'Fizz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(15)).to eq 'FizzBuzz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "1" when passed 1' do
+    expect(fizzbuzz(1)).to eq 1
   end
 end
